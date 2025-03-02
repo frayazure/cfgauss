fcf5160763ae1f80514bafa30b911b186a61ace4
Cesar Quispe <fray.azure@gmail.com>
2025-03-01 19:10:45 -0500

    desarrollando en la rama dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="flex justify-center text-3xl">Bienvenido al colegio Gauss</h1>
+      <h2 className="flex justify-center text-xl">Matrículas abiertas</h2>
     </div>
   );
 }
